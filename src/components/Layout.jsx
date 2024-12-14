91dea78399e8fe7217497dc32b6ddeddc0c8e57c
Aayush <aausgh@gmail.com>
2024-12-14 13:41:08 +0545

    added layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+export default function Layout({ children }) {
+   const header = (
+      <header></header>
+   )
+   const footer = (
+      <footer></footer>
+   )
+   return (
+      <>
+         {header}
+         <main>{children}</main>
+         {footer}
+      </>
+   )
+}
